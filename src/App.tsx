ec8bf597406a716b864c2a40759bc2df48f32495
임장순 <rosenari88@gmail.com>
2021-04-13 17:56:13 +0900

    Config / typescript env

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Styled from "styled-components/native";
+import type { ReactNode } from 'react'
+
+const Container = Styled.View`
+  flex:1;
+  background-color:#EEE;
+`;
+
+const App: () => ReactNode = () => {
+  return (
+    <Container>
+
+    </Container>
+  );
+};
+
+export default App;
